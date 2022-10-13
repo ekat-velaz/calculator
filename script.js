e22ec06fe08ebd7ac5d761ebd503f2e6cd6d7017
ekaterina <katyabalash@mail.ru>
2022-10-13 17:36:16 +0300

    Build calculator's HTML and DOM

---
+++ script.js
@@ -0,0 +1,86 @@
+const inputScreen = document.querySelector(".input");
+const resultScreen = document.querySelector(".result");
+const btnClear = document.querySelector("#btn-clear");
+const btnDelete = document.querySelector("#btn-delete");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn6");
+const btn7 = document.querySelector("#btn7");
+const btn8 = document.querySelector("#btn8");
+const btn9 = document.querySelector("#btn9");
+const btn0 = document.querySelector("#btn0");
+const btnAdd = document.querySelector("#btn-add");
+const btnSubstract = document.querySelector("#btn-substract");
+const btnMultiply = document.querySelector("#btn-multiply");
+const btnDivide = document.querySelector("#btn-divide");
+const btnEquals = document.querySelector("#btn-equals");
+const btnDot = document.querySelector("#btn-dot");
+
+btn1.addEventListener("click", () => {
+   inputScreen.textContent =  `${inputScreen.textContent}1`;
+});
+
+btn2.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}2`;
+ });
+
+ btn3.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}3`;
+ });
+
+ btn4.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}4`;
+ });
+
+ btn5.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}5`;
+ });
+
+ btn6.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}6`;
+ });
+
+ btn7.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}7`;
+ });
+
+ btn8.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}8`;
+ });
+
+ btn9.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}9`;
+ });
+
+ btn0.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}0`;
+ });
+
+ btnAdd.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}+`;
+ });
+
+ btnSubstract.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}-`;
+ });
+
+ btnMultiply.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}x`;
+ });
+
+ btnDivide.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}/`;
+ });
+
+ btnEquals.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}=`;
+ });
+
+ btnDot.addEventListener("click", () => {
+    inputScreen.textContent =  `${inputScreen.textContent}.`;
+ });
+
+ 
