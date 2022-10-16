00734dce5ada724bcd9fd8bb9c8440f5eb218067
ekaterina <katyabalash@mail.ru>
2022-10-16 13:26:20 +0300

    Fix bugs

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let b;
 let arr;
 let result;
 let arrayScreen;
+let arrLength;
+let stringInput = "";
+let stringPart;
+let stringStart;
 
 const inputScreen = document.querySelector(".input");
 const resultScreen = document.querySelector(".result");
@@ -27,6 +31,8 @@ const btnDot = document.querySelector("#btn-dot");
 
 btnClear.addEventListener("click", () => {
    inputScreen.textContent = "";
+   stringInput = "";
+   btnDot.disabled = false;
 });
 
 btnDelete.addEventListener("click", () => {
@@ -35,136 +41,215 @@ btnDelete.addEventListener("click", () => {
     let poppedOut = arrayScreen.pop();
     arrayScreen = arrayScreen.join("");
 inputScreen.textContent = arrayScreen;
+
+stringInput = stringStart.toString();
+stringInput = stringInput.split('"');
+poppedOut = stringInput.pop();
+stringInput = stringInput.join('"');
+
+btnDot.disabled = false;
  });
 
 btn1.addEventListener("click", () => {
    inputScreen.textContent =  `${inputScreen.textContent}1`;
+   stringInput = `${stringInput}1`;
 });
 
 btn2.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}2`;
+    stringInput = `${stringInput}2`;
  });
 
  btn3.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}3`;
+    stringInput = `${stringInput}3`;
  });
 
  btn4.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}4`;
+    stringInput = `${stringInput}4`;
  });
 
  btn5.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}5`;
+    stringInput = `${stringInput}5`;
  });
 
  btn6.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}6`;
+    stringInput = `${stringInput}6`;
  });
 
  btn7.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}7`;
+    stringInput = `${stringInput}7`;
  });
 
  btn8.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}8`;
+    stringInput = `${stringInput}8`;
  });
 
  btn9.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}9`;
+    stringInput = `${stringInput}9`;
  });
 
  btn0.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}0`;
+    stringInput = `${stringInput}0`;
  });
 
  btnAdd.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}+`;
+    stringInput = `${stringInput}"+"`;
+    btnDot.disabled = false;
  });
 
  btnSubstract.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}-`;
+    stringInput = `${stringInput}"-"`;
+    btnDot.disabled = false;
  });
 
  btnMultiply.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}x`;
+    stringInput = `${stringInput}"x"`;
+    btnDot.disabled = false;
  });
 
  btnDivide.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}/`;
+    stringInput = `${stringInput}"/"`;
+    btnDot.disabled = false;
  });
 
  btnEquals.addEventListener("click", () => {
-    
+    splitInput();
     operate();
+    btnDot.disabled = false;
  });
 
  btnDot.addEventListener("click", () => {
     inputScreen.textContent =  `${inputScreen.textContent}.`;
+    stringInput = `${stringInput}.`;
+    btnDot.disabled = true;
  });
 
  function add(a,b) {
     result = a+b;
+    result = Math.round(result*100)/100;
     resultScreen.textContent = result;
  };
 
  function substract(a,b) {
     result = a - b;
+    result = Math.round(result*100)/100;
     resultScreen.textContent = result;
  };
 
  function  multiply(a,b) {
     result = a * b;
+    result = Math.round(result*100)/100;
     resultScreen.textContent = result;
  };
 
  function divide(a,b) {
     result = a / b;
+    result = Math.round(result*100)/100;
     resultScreen.textContent = result;
  };
 
+function splitInput() {
+    arr = inputScreen.textContent;
+    arr = arr.split("+");
+    arr = arr.join(" ")
+    arr = arr.split("-");
+    arr = arr.join(" ");
+    arr = arr.split("x");
+    arr = arr.join(" ");
+    arr = arr.split("/");
+    arr = arr.join(" ");
+    arr = arr.split(" ");
+    console.log(arr);
+};
+
 function getAandB() {
-
-    if (inputScreen.textContent.includes("+")) {
-        arr = inputScreen.textContent.split("+");
-
-    } else if (inputScreen.textContent.includes("-")) {
-        arr = inputScreen.textContent.split("-");
-
-    } else if (inputScreen.textContent.includes("x")) {
-        arr = inputScreen.textContent.split("x");
-
-    } else if (inputScreen.textContent.includes("/")) {
-        arr = inputScreen.textContent.split("/");
-    };
-  
     a = arr.slice(0,1);
     b = arr.slice(1,2);
     console.log(a,b);
+    
+    arr = arr.slice();
+    console.log(arr);
 
     a = a.toString();
     b = b.toString();
-    console.log(a,b);
 
     a = +a;
     b = +b;
     console.log(a,b);
 };
 
+function chooseOperation() {
+   if (stringPart === "+") {
+      add(a,b);
+  } else if (stringPart === "-") {
+      substract(a,b);
+  } else if (stringPart === "x") {
+      multiply(a,b);
+  } else if (stringPart === "/") {
+      divide(a,b);
+  };
+};
+
+function getAandBfromResult() {
+ a = result;
+ b = arr.slice(0,1);
+ console.log(a,b);
+    
+    a = a.toString();
+    b = b.toString();
+    a = +a;
+    b = +b;
+    console.log(a,b);
+
+};
 
  function operate() {
-    
+stringStart = stringInput;
+
+   stringInput = stringInput.toString();
+   console.log(stringInput);
+   stringInput = stringInput.split('"');
+
+   
     getAandB();
-
-
-    if (inputScreen.textContent.includes("+")) {
-        add(a,b);
-    } else if (inputScreen.textContent.includes("-")) {
-        substract(a,b);
-    } else if (inputScreen.textContent.includes("x")) {
-        multiply(a,b);
-    } else if (inputScreen.textContent.includes("+")) {
-        divide(a,b);
-    };
-
+    stringInput.shift();
+    getStringPart();
+    chooseOperation();
     
+
+console.log(result);
+arr.shift();
+
+for (let i=0; i <= arr.length; i++) {
+   stringInput.shift();
+   stringInput.shift();
+   getStringPart();
+   arr.shift();
+ if (arr.length >= 1) {
+   console.log(arr);
+   getAandBfromResult();
+   chooseOperation();
+   console.log(result);
+   
  };
+};
+    
+};
+
+function getStringPart() {
+   stringPart = stringInput.slice(0,1);
+   stringPart = stringPart.toString();
+   console.log(stringPart);
+};
